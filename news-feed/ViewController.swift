00c2e62ad4f8b7d6a71b3f9cd540d7929055f5f7
Nijat Hamid <nicatorium@gmail.com>
2024-11-19 13:31:56 +0400

    Initial Commit

---
+++ news-feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news-feed
+//
+//  Created by Nijat Hamid on 11/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
